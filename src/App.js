18c92a4e64d772763035f4b15aaa7438102665d0
emmanuelguty <manuel15901@gmail.com>
2022-12-19 21:13:46 -0600

    Se termino de hacer el frontend de login y register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Register from './pages/Register';
+//import Register from './pages/Register';
+//import Login from './pages/Login';
+import Home from './pages/Home';
 
 //Estilos en Sass
 import "./style.scss";
 
 function App() {
   return (
-    <Register />
+    <Home />
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import Add from '../images/add_images.png';
 
 const Register = () => {
     return(
         <div className='formContainer'>
             <div className='formWrapper'>
                 <span className='logo'>GART TUBE</span>
-                <span className='title'>GART TUBE</span>
+                <span className='title'>Ingresa a Gart Tube</span>
 
                 <form>
                     <input type="text" placeholder='display name' />
                     <input type="email" placeholder='email'/>
                     <input type="password" placeholder='password'/>
-                    <input type="file" />
+                    <input style={{display:"none"}} type="file" />
+                    <label htmlFor="file">
+                        <img src={Add} alt="Agregar imagen" />
+                        <span>Add an avatar</span>
+                    </label>
                     <button>Sign up</button>
                 </form>
                 <p>You do have an account? Login</p>
